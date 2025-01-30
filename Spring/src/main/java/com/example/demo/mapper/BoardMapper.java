3317433e44d958162f481d1c56b07b7419dbbad4
sunbeekim <rlatjsql12@gmail.com>
2025-01-30 11:05:46 +0900

    차트수정 및 분리

--- Spring/src/main/java/com/example/demo/mapper/BoardMapper.java
+++ Spring/src/main/java/com/example/demo/mapper/BoardMapper.java
@@ -9,7 +9,8 @@ import java.util.List;
 @Mapper
 public interface BoardMapper {
 
-  // 모든 게시물 조회
+  // 데이터베이스에서 모든 게시물을 조회하는 SQL 쿼리를 실행합니다.
+  // 조회된 각 행(row)은 Board 객체로 매핑되며, 결과는 Board 객체로 이루어진 리스트(List<Board>)로 반환됩니다.
   List<Board> findAll();
 
   // ID로 게시물 조회
